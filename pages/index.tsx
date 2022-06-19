072f30811d9c1e3dc63b4663e8d3141bc1e11bf0
SlimyZockt <35144741+SlimyZockt@users.noreply.github.com>
2022-06-19 15:48:08 +0200

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,9 @@ const Home: NextPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const sendData = async (event: any) => {
+  const sendData = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
 
     let res = await fetch('/api/data', {
       method: 'POST',
@@ -33,29 +35,30 @@ const Home: NextPage = () => {
   return (
     <Container>
       <Stack gap={4}>
-      <Nbar />
-      <Container fluid className='bg-2 text-center'>
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" onChange={(e: any) => setEmail(e.target.value)} />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+        <Nbar />
+        <Container fluid className='bg-2 text-center'>
+          <Form onSubmit={sendData}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" onChange={(e: any) => setEmail(e.target.value)} required />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" onChange={(e: any) => setPassword(e.target.value)}/>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-            <Button variant="primary" type="button" onClick={(e: any) => sendData(e)}>
-            Submit
-          </Button>
-        </Form>
-      </Container>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" onChange={(e: any) => setPassword(e.target.value)} required />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Label>AGB</Form.Label>
+              <Form.Check type="checkbox" label="Check me out" required />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Container>
       </Stack>
 
 
